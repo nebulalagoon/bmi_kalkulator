525f622461e43497ca5f1d315bc4157c8747a83c
nebulalagoon <atramhenc@gmail.com>
2019-02-10 20:05:09 +0100

    First commit
    Importing existing project

---
+++ js/BMI_kalkulator.js
@@ -0,0 +1,19 @@
+function bmi() {
+
+   var d=document.querySelector('body');
+   var r=d.querySelector('#rezultat');
+
+    if (r!==null)
+        d.removeChild(r);
+  
+  var m=document.querySelector('#masa_inp').value;
+  var v=document.querySelector('#visina_inp').value/100;
+
+    var c=m/(v*v);
+    var rez=document.createElement('section');
+    rez.textContent='Vaš BMI iznosi '+ c.toFixed(2) +'.';
+    rez.id='rezultat';
+    document.body.append(rez); 
+}
+
+document.querySelector('#gumb').addEventListener('click', bmi, false);
