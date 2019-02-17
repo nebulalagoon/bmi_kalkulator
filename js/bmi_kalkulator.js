9ad6e02e6c4a2a66229368a2886fb32b8769ae8f
nebulalagoon <atramhenc@gmail.com>
2019-02-17 21:56:46 +0100

    Optimised JavaScript, added checks for invalid input

--- js/bmi_kalkulator.js
+++ js/bmi_kalkulator.js
@@ -1,19 +1,36 @@
-function bmi() {
+const bmi = (m,v) => {
+   return m/(v*v); 
+  }
 
-   var d=document.querySelector('body');
-   var r=d.querySelector('#rezultat');
+function racun() {
 
-    if (r!==null)
+  const d = document.querySelector('body'); //micanje starog rezultata
+  const r = d.querySelector('.rezultat');
+
+ if (r) {
         d.removeChild(r);
-  
-  var m=document.querySelector('#masa_inp').value;
-  var v=document.querySelector('#visina_inp').value/100;
+    };
 
-    var c=m/(v*v);
-    var rez=document.createElement('section');
-    rez.textContent='Vaš BMI iznosi '+ c.toFixed(1) +'.';
-    rez.id='rezultat';
+  let masaInput = document.querySelector('#masa_inp').value;
+  let visinaInput = document.querySelector('#visina_inp').value/100;
+
+
+  if ((masaInput === 0) || (visinaInput === 0)) {
+    const pog = document.createElement('section'); //dodavanje greške
+    pog.textContent = 'Molim unesite valjane podatke.';
+    pog.className = 'rezultat';
+    pog.id = 'greska';
+    document.body.append(pog);
+  } else {
+    let c = bmi(masaInput, visinaInput);
+    const rez = document.createElement('section'); //dodavanje novog elementa
+    rez.textContent = 'Vaš BMI iznosi '+ c.toFixed(1) +'.';
+    rez.className = 'rezultat';
     document.body.append(rez);
-}
+  }
+};
 
-document.querySelector('#gumb').addEventListener('click', bmi, false);
+document.querySelector('#gumb').addEventListener('click', racun, false);
+
+
+
