5bf4265680b79b34a53e1781d393b0a85dd33ca1
nebulalagoon <atramhenc@gmail.com>
2019-03-06 13:27:01 +0100

    Added age condition

--- js/bmi_kalkulator.js
+++ js/bmi_kalkulator.js
@@ -1,7 +1,10 @@
+//funkcija za racunanje bmi-ja
 const bmi = (m,v) => {
   return m/(v*v); 
  }
 
+
+//callback funkcija
 function racun() {
 
  const d = document.querySelector('body'); //micanje starog rezultata
@@ -13,7 +16,7 @@ while (r) {
 
  let masaInput = document.querySelector('#masa_inp').value;
  let visinaInput = document.querySelector('#visina_inp').value/100;
-
+ let dobInput = document.querySelector('#dob_inp').value;
 
  if ((masaInput <= 0) || (visinaInput <= 0)) {
    const pog = document.createElement('section'); //dodavanje greške
@@ -21,6 +24,12 @@ while (r) {
    pog.className = 'rezultat';
    pog.id = 'greska';
    document.body.appendChild(pog);
+ } else if (dobInput < 18) {
+  const upozorenje = document.createElement('section');
+  upozorenje.textContent = 'BMI nije pogodan alat za procjenjivanje uhranjenosti djece i adolescenata mlađih od 18 godina';
+  upozorenje.className = 'rezultat';
+  upozorenje.id = 'upozorenje';
+  document.body.appendChild(upozorenje);
  } else {
    let c = bmi(masaInput, visinaInput);
    const rez = document.createElement('section'); //dodavanje novog elementa
@@ -28,6 +37,9 @@ while (r) {
    rez.className = 'rezultat';
    document.body.appendChild(rez);
  }
+
 };
 
-document.querySelector('#gumb').addEventListener('click', racun, false);
+
+//gumb - event listener
+document.querySelector('#gumb').addEventListener('click', racun, false); 
