eb45b0ab30c0cb59bf93f589c11094165d5b9bff
nebulalagoon <atramhenc@gmail.com>
2019-03-06 13:17:25 +0100

    Minor script and css change

--- js/bmi_kalkulator.js
+++ js/bmi_kalkulator.js
@@ -1,33 +1,33 @@
 const bmi = (m,v) => {
-   return m/(v*v); 
-  }
+  return m/(v*v); 
+ }
 
 function racun() {
 
-  const d = document.querySelector('body'); //micanje starog rezultata
-  const r = d.querySelector('.rezultat');
+ const d = document.querySelector('body'); //micanje starog rezultata
+ const r = d.querySelector('.rezultat');
 
- if (r) {
-        d.removeChild(r);
-    };
+while (r) {
+       d.removeChild(r);
+   };
 
-  let masaInput = document.querySelector('#masa_inp').value;
-  let visinaInput = document.querySelector('#visina_inp').value/100;
+ let masaInput = document.querySelector('#masa_inp').value;
+ let visinaInput = document.querySelector('#visina_inp').value/100;
 
 
-  if ((masaInput <= 0) || (visinaInput <= 0)) {
-    const pog = document.createElement('section'); //dodavanje greške
-    pog.textContent = 'Molim unesite valjane podatke.';
-    pog.className = 'rezultat';
-    pog.id = 'greska';
-    document.body.appendChild(pog);
-  } else {
-    let c = bmi(masaInput, visinaInput);
-    const rez = document.createElement('section'); //dodavanje novog elementa
-    rez.textContent = 'Vaš BMI iznosi '+ c.toFixed(1) +'.';
-    rez.className = 'rezultat';
-    document.body.appendChild(rez);
-  }
+ if ((masaInput <= 0) || (visinaInput <= 0)) {
+   const pog = document.createElement('section'); //dodavanje greške
+   pog.textContent = 'Molim unesite valjane podatke.';
+   pog.className = 'rezultat';
+   pog.id = 'greska';
+   document.body.appendChild(pog);
+ } else {
+   let c = bmi(masaInput, visinaInput);
+   const rez = document.createElement('section'); //dodavanje novog elementa
+   rez.textContent = 'Vaš BMI iznosi '+ c.toFixed(1) +'.';
+   rez.className = 'rezultat';
+   document.body.appendChild(rez);
+ }
 };
 
 document.querySelector('#gumb').addEventListener('click', racun, false);
